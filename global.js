d4a77c61636c946ab398b54b2307e716e7a716a8
Co-7 <colin.v42@protonmail.com>
2020-04-12 13:54:22 +0200

    Code optimization

--- global.js
+++ global.js
@@ -1,3 +1,54 @@
-function test() {
-    console.log("cei est un test");
+//Variable
+let valueBgColor;
+let valueTextColor;
+let arrayLetter = [];
+let letterBox = document.querySelector('#letter-box');
+
+//Variable Default Value
+let valueBgColorDefault = "#4b4b4b";
+let valueTextColorDefault = "#ffffff";
+let arrayLetterDefault = ["À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "Ç", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï", "Ð", "Ñ", "Ò", "Ó", "Ô", "Õ", "Ö", "Ø", "Œ", "Š", "þ", "Ù", "Ú", "Û", "Ü", "Ý", "Ÿ"];
+
+//Function For Clear Array in HTML
+function clearArray() {
+    let tr = document.querySelectorAll('tr');
+    for (let i = 0; i < tr.length; i++) {
+        letterBox.removeChild(tr[i]);
+    }
+}
+
+function makeArray() {
+    let nb = Math.ceil(arrayLetter.length) / numberLetterByLine;
+    for (let i = nb; i > 0; i--) {
+        let tr = document.createElement('tr');
+        letterBox.appendChild(tr);
+    }
+    let j = 0;
+    let y = 0;
+    for (let letter of arrayLetter) {
+
+        if (j === numberLetterByLine) {
+            y++;
+            j = 0;
+        }
+        j++;
+        let tr = document.querySelectorAll('tr')[y];
+        let td = document.createElement('td');
+        td.addEventListener('click', function () {
+            let textarea = document.createElement("textarea");
+            textarea.value = this.textContent;
+            document.body.appendChild(textarea);
+            textarea.select();
+            document.execCommand("copy");
+            document.body.removeChild(textarea);
+        });
+        td.textContent = letter;
+        tr.appendChild(td);
+    }
+}
+
+function setValueVariable(result) {
+    valueBgColor = result.storageBgColor === undefined ? valueBgColorDefault : result.storageBgColor;
+    valueTextColor = result.storageTextColor === undefined ? valueTextColorDefault : result.storageTextColor;
+    arrayLetter = result.storageLetter === undefined || result.storageLetter.length === 0 ? arrayLetterDefault : result.storageLetter;
 }
--- options.js
+++ options.js
@@ -5,13 +5,16 @@ let btnTextColorInput = document.querySelector("#btn-text-color-input");
 let btnTextColorSpan = document.querySelector("#btn-text-color-span");
 let designPreview = document.querySelector("#design-preview");
 let reset = document.querySelector('#reset');
-let valueBgColor;
-let valueTextColor;
-let arrayLetter = [];
+let btnDelete = document.querySelector('#btn-delete');
+let btnAdd = document.querySelector('#btn-add');
+let inputTextAdd = document.querySelector('#input-text-add');
+let btnStopDelete = document.querySelector('#btn-stop-delete');
+let numberLetterByLine = 10; //Number of Letters displayed per line
+
 
 btnBgColorInput.addEventListener('change', function () {
     console.log("Couleur Selectionné : " + btnBgColorInput.value);
-    let valueBgColor = btnBgColorInput.value;
+    valueBgColor = btnBgColorInput.value;
     btnBgColorSpan.style.backgroundColor = valueBgColor;
     designPreview.style.backgroundColor = valueBgColor;
     storageSetBgColor();
@@ -27,115 +30,98 @@ btnTextColorInput.addEventListener('change', function () {
 });
 
 reset.addEventListener('click', function () {
-    chrome.storage.sync.clear();
-    console.log("Storage clear");
-    clearArray();
-    storageGet();
+
+    let result = confirm("Attention tous les parametres vont être reinitialisé ! Etes vous certains ?");
+
+    if (result) {
+        chrome.storage.sync.clear();
+        console.log("Storage clear");
+        clearArray();
+        storageGet();
+    }
 });
 
-function clearArray() {
-    let tr = document.querySelectorAll('tr');
-    for (let i = 0; i < tr.length; i++) {
-        letterBox.removeChild(tr[i]);
-    }
-}
-
 function storageSetBgColor() {
-    console.log("Exec storageSet()");
     chrome.storage.sync.set({storageBgColor: valueBgColor}, function () {
         console.log('valueBgColor est defini sur : ' + valueBgColor);
     });
 }
 
 function storageSetTextColor() {
-    console.log("Exec storageSet()");
     chrome.storage.sync.set({storageTextColor: valueTextColor}, function () {
         console.log('valueTextColor est defini sur : ' + valueTextColor);
     });
 }
 
 function storageSetArrayLetter() {
-    console.log("Exec storageSet()");
     chrome.storage.sync.set({storageLetter: arrayLetter}, function () {
-        console.log('arrayLetter est defini sur : ' + arrayLetter);
+        console.log(arrayLetter);
     });
     storageGet();
+}
+
+function updateStyleHtmlOption() {
+    btnBgColorInput.value = valueBgColor;
+    btnBgColorSpan.style.backgroundColor = valueBgColor;
+    designPreview.style.backgroundColor = valueBgColor;
+    btnTextColorInput.value = valueTextColor;
+    btnTextColorSpan.style.backgroundColor = valueTextColor;
+    designPreview.style.color = valueTextColor;
 }
 
 function storageGet() {
     clearArray();
     chrome.storage.sync.get(null, function (result) {
-
-        valueBgColor = result.storageBgColor === undefined ? "#000000" : result.storageBgColor;
-        valueTextColor = result.storageTextColor === undefined ? "#ffffff" : result.storageTextColor;
-        arrayLetter = result.storageLetter === undefined || result.storageLetter.length === 0 ? ['À', 'É', 'È', 'Ù', 'Æ', 'æ', 'Œ', 'œ'] : result.storageLetter;
-        btnBgColorInput.value = valueBgColor;
-        btnBgColorSpan.style.backgroundColor = valueBgColor;
-        designPreview.style.backgroundColor = valueBgColor;
-        btnTextColorInput.value = valueTextColor;
-        btnTextColorSpan.style.backgroundColor = valueTextColor;
-        designPreview.style.color = valueTextColor;
-        makeArray()
+        setValueVariable(result);
+        updateStyleHtmlOption();
+        makeArray();
     });
-}
 
-let letterBox = document.querySelector('#letter-box');
-let btnDelete = document.querySelector('#btn-delete');
-let btnAdd = document.querySelector('#btn-add');
-let inputTextAdd = document.querySelector('#input-text-add');
-
-
-let btnCleanListner = document.querySelector('#btn-cleanListner');
-
-function makeArray() {
-    let nb = Math.ceil(arrayLetter.length) / 4;
-    for (let i = nb; i > 0; i--) {
-        let tr = document.createElement('tr');
-        letterBox.appendChild(tr);
-    }
-    let j = 0;
-    let y = 0;
-    for (let letter of arrayLetter) {
-        if (j === 4) {
-            y++;
-            j = 0;
-        }
-        j++;
-        let tr = document.querySelectorAll('tr')[y];
-        let td = document.createElement('td');
-        td.textContent = letter;
-        tr.appendChild(td);
-    }
 }
 
 
 btnDelete.addEventListener('click', function () {
     let nbTd = document.querySelectorAll("td");
     btnDelete.classList.add("d-none");
-    btnCleanListner.classList.remove("d-none");
+    btnAdd.classList.add("d-none");
+    inputTextAdd.classList.add("d-none");
+    btnStopDelete.classList.remove("d-none");
 
     for (let i = 0; i < nbTd.length; i++) {
         let td = document.querySelectorAll("td")[i];
         td.addEventListener('click', del)
     }
-
 });
 
 btnAdd.addEventListener('click', function () {
-    arrayLetter.push(inputTextAdd.value);
-    storageSetArrayLetter()
+    if (inputTextAdd.value !== "" && inputTextAdd.value !== " " && inputTextAdd.value.length === 1) {
+        let j = false;
+
+        for (let letter of arrayLetter) {
+            j = letter === inputTextAdd.value ? j = true : j;
+        }
+
+        if (j !== true) {
+            arrayLetter.push(inputTextAdd.value);
+            inputTextAdd.value = "";
+            storageSetArrayLetter()
+        }
+
+
+    }
 });
 
 
-btnCleanListner.addEventListener('click', function () {
+btnStopDelete.addEventListener('click', function () {
     let newArray = [];
     let nbTd = document.querySelectorAll("td")
     btnDelete.classList.remove("d-none");
-    btnCleanListner.classList.add("d-none");
+    btnAdd.classList.remove("d-none");
+    inputTextAdd.classList.remove("d-none");
+    btnStopDelete.classList.add("d-none");
 
     for (let i = 0; i < nbTd.length; i++) {
         let td = document.querySelectorAll("td")[i];
-        console.log(td);
         td.removeEventListener('click', del);
         newArray.push(td.innerText);
     }
@@ -145,6 +131,6 @@ btnCleanListner.addEventListener('click', function () {
 
 
 function del() {
-    console.log("delete has been Delete");
+    console.log("Letter has been Delete");
     this.remove();
 }
