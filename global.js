e3890d0c0c079880c4ac12a89763dbefe661b15e
Co-7 <colin.v42@protonmail.com>
2020-04-13 14:25:44 +0200

    Code optimization and add Characters

--- global.js
+++ global.js
@@ -2,30 +2,21 @@
 let valueBgColor;
 let valueTextColor;
 let letterBox = document.querySelector('#letter-box');
-let arrayAlphabet = [];
-let arrayGreekCharacters = [];
-let arrayCyrillicCharacters = [];
-let arrayHebrewCharacters = [];
-let arrayPhoneticCharacters = [];
-let arrayBrailleCharacter = [];
-let arrayOghamCharacters = [];
-let arrayRunesCharacters = [];
-let arrayThaiCharacters = [];
-let arrayMathCharacters = [];
 
 //Variable Default Value
 let valueBgColorDefault = "#4b4b4b";
 let valueTextColorDefault = "#ffffff";
-let arrayAlphabetDefault = ["À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "Ç", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï", "Ð", "Ñ", "Ò", "Ó", "Ô", "Õ", "Ö", "Ø", "Œ", "Š", "þ", "Ù", "Ú", "Û", "Ü", "Ý", "Ÿ"];
-let arrayGreekCharactersDefault = ["α", "Α", "β", "Β", "γ", "Γ", "ε", "ϵ", "θ", "ϑ", "κ", "ϰ", "π", "ϖ", "ρ", "ϱ", "σ", "ς", "ϕ", "φ", "ω", "Ω", "℧", "ϝ", "Ϝ"];
-let arrayCyrillicCharactersDefault = ["Ѐ", "Ё", "Ђ", "Ѓ", "Є", "Ѕ", "І", "Ї", "Ј", "Љ", "Њ"];
-let arrayPhoneticCharactersDefault = ["ɐ", "ɑ", "ɒ", "ɓ", "ɔ", "ɕ", "ɖ", "ɗ", "ɘ", "ə", "ɚ"];
-let arrayBrailleCharacterDefault = ["⠀", "⠁", "⠂", "⠃", "⠄", "⠅", "⠆", "⠇"];
-let arrayOghamCharactersDefault = [" ", "ᚁ", "ᚂ", "ᚃ", "ᚄ", "ᚅ", "ᚆ", "ᚇ", "ᚈ", "ᚉ", "ᚊ", "ᚋ", "ᚌ", "ᚍ", "ᚎ", "ᚏ", "ᚐ", "ᚑ", "ᚒ", "ᚓ", "ᚔ", "ᚕ", "ᚖ", "ᚗ", "ᚘ", "ᚙ", "ᚚ", "᚛", "᚜"];
-let arrayRunesCharactersDefault = ["ᚠ", "ᚡ", "ᚢ", "ᚣ", "ᚤ", "ᚥ", "ᚦ", "ᚧ", "ᚨ", "ᚩ", "ᚪ", "ᚫ"];
-let arrayThaiCharactersDefault = ["ก", "ข", "ฃ", "ค", "ฅ", "ฆ", "ง", "จ", "ฉ", "ช"];
-let arrayMathCharactersDefault = ["¼", "½", "¾", "↉", "⅓", "⅔", "⅕", "⅖", "⅗", "⅘", "⅙", "⅚", "⅛", "⅜", "⅝", "⅞", "⅟", "⅐", "⅑", "⅒", "Ⅰ", "Ⅱ", "Ⅲ", "Ⅳ", "Ⅴ", "Ⅵ", "Ⅶ", "Ⅷ", "Ⅸ", "Ⅹ", "Ⅺ", "Ⅻ", "Ⅼ", "Ⅽ", "Ⅾ", "Ⅿ", "ↀ", "ↁ", "ↂ", "Ↄ", "ↇ", "ↈ", "ⅰ", "ⅱ", "ⅲ", "ⅳ", "ⅴ", "ⅵ", "ⅶ", "ⅷ", "ⅸ", "ⅹ", "ⅺ", "ⅻ", "ⅼ", "ⅽ", "ⅾ", "ⅿ", "ↄ", "ℂ", "ℍ", "ℕ", "ℙ", "ℚ", "ℝ", "ℤ", "∅"];
-let arraySelect = arrayAlphabetDefault;
+arrayAlphabetCharacters = ["À", "Á", "Â", "Ã", "Ä", "Å", "Æ", "Ç", "È", "É", "Ê", "Ë", "Ì", "Í", "Î", "Ï", "Ð", "Ñ", "Ò", "Ó", "Ô", "Õ", "Ö", "Ø", "Œ", "Š", "þ", "Ù", "Ú", "Û", "Ü", "Ý", "Ÿ"];
+arrayGrecCharacters = ["α", "Α", "β", "Β", "γ", "Γ", "ε", "ϵ", "θ", "ϑ", "κ", "ϰ", "π", "ϖ", "ρ", "ϱ", "σ", "ς", "ϕ", "φ", "ω", "Ω", "℧", "ϝ", "Ϝ"];
+arrayCyrilliqueCharacters = ["Ѐ", "Ё", "Ђ", "Ѓ", "Є", "Ѕ", "І", "Ї", "Ј", "Љ", "Њ"];
+arrayHebraiqueCharacters = ["א", "ב", "ג", "ד"];
+arrayPhonetiqueCharacters = ["ɐ", "ɑ", "ɒ", "ɓ", "ɔ", "ɕ", "ɖ", "ɗ", "ɘ", "ə", "ɚ"];
+arrayBrailleCharacters = ["⠁", "⠂", "⠃", "⠄", "⠅", "⠆", "⠇"];
+arrayOghamiqueCharacters = [" ", "ᚁ", "ᚂ", "ᚃ", "ᚄ", "ᚅ", "ᚆ", "ᚇ", "ᚈ", "ᚉ", "ᚊ", "ᚋ", "ᚌ", "ᚍ", "ᚎ", "ᚏ", "ᚐ", "ᚑ", "ᚒ", "ᚓ", "ᚔ", "ᚕ", "ᚖ", "ᚗ", "ᚘ", "ᚙ", "ᚚ", "᚛", "᚜"];
+arrayRuniqueCharacters = ["ᚠ", "ᚡ", "ᚢ", "ᚣ", "ᚤ", "ᚥ", "ᚦ", "ᚧ", "ᚨ", "ᚩ", "ᚪ", "ᚫ"];
+arrayThaiCharacters = ["ก", "ข", "ฃ", "ค", "ฅ", "ฆ", "ง", "จ", "ฉ", "ช"];
+arrayMathCharacters = ["¼", "½", "¾", "↉", "⅓", "⅔", "⅕", "⅖", "⅗", "⅘", "⅙", "⅚", "⅛", "⅜", "⅝", "⅞", "⅟", "⅐", "⅑", "⅒", "Ⅰ", "Ⅱ", "Ⅲ", "Ⅳ", "Ⅴ", "Ⅵ", "Ⅶ", "Ⅷ", "Ⅸ", "Ⅹ", "Ⅺ", "Ⅻ", "Ⅼ", "Ⅽ", "Ⅾ", "Ⅿ", "ↀ", "ↁ", "ↂ", "Ↄ", "ↇ", "ↈ", "ⅰ", "ⅱ", "ⅲ", "ⅳ", "ⅴ", "ⅵ", "ⅶ", "ⅷ", "ⅸ", "ⅹ", "ⅺ", "ⅻ", "ⅼ", "ⅽ", "ⅾ", "ⅿ", "ↄ", "ℂ", "ℍ", "ℕ", "ℙ", "ℚ", "ℝ", "ℤ", "∅"];
+let arraySelect = arrayAlphabetCharacters;
 
 //Function For Clear Array in HTML
 function clearArray() {
@@ -68,5 +59,5 @@ function makeArray(array) {
 function setValueVariable(result) {
     valueBgColor = result.storageBgColor === undefined ? valueBgColorDefault : result.storageBgColor;
     valueTextColor = result.storageTextColor === undefined ? valueTextColorDefault : result.storageTextColor;
-    arrayAlphabet = result.storageLetter === undefined || result.storageLetter.length === 0 ? arrayAlphabetDefault : result.storageLetter;
+    // arrayAlphabet = result.storageLetter === undefined || result.storageLetter.length === 0 ? arrayAlphabetDefault : result.storageLetter;
 }
--- tabs-characters.js
+++ tabs-characters.js
@@ -1,10 +1,35 @@
+let ul = document.querySelector("#navbar-tab-character");
+let navTabCharacter = ['Alphabet', 'Grec', 'Cyrillique', 'Hebraique', 'Phonetique' , 'Braille' , 'Oghamique' , 'Runique' , 'Thai' , 'Math'];
+let arrayTabsCharacters = [];
+let arrayArrayCharacters = [];
+createNavTabCharacter();
 let allCharactersTabs = document.querySelectorAll('*[id^="characters-tab-"]');
-let tabsAlphabetCharacters = document.querySelector("#characters-tab-alphabet");
-let tabsGreekCharacters = document.querySelector("#characters-tab-greek");
-let tabsCyrillicCharacters = document.querySelector("#characters-tab-cyrillic");
-let arrayTabsCharacters = [tabsAlphabetCharacters, tabsGreekCharacters, tabsCyrillicCharacters];
-let arrayArrayCharacters = [arrayAlphabetDefault, arrayGreekCharactersDefault, arrayCyrillicCharactersDefault];
+
 let i = 0;
+
+function createNavTabCharacter() {
+
+    for (let nav of navTabCharacter) {
+        let li = document.createElement("li");
+        li.classList.add("nav-item");
+        ul.appendChild(li);
+        let a = document.createElement("a");
+        a.classList.add("nav-link");
+        a.innerText = nav;
+        let nameVarInArrayTabsCharacters = window["tabs" + nav + "Characters"];
+        let nameVarInArrayArrayCharacters = window["array" + nav + "Characters"];
+        console.log(nameVarInArrayArrayCharacters);
+        nav = nav.toLowerCase();
+        a.id = "characters-tab-" + nav;
+        a.href = "#";
+        li.appendChild(a);
+        nameVarInArrayTabsCharacters = document.querySelector("#" + `${a.id}`);
+        arrayTabsCharacters.push(nameVarInArrayTabsCharacters);
+        arrayArrayCharacters.push(nameVarInArrayArrayCharacters);
+    }
+    console.log(arrayArrayCharacters);
+    document.querySelector("#characters-tab-alphabet").classList.add("active");
+}
 
 for (let tabCharacter of arrayTabsCharacters) {
     let j = i;
@@ -14,10 +39,6 @@ for (let tabCharacter of arrayTabsCharacters) {
         arraySelect = arrayArrayCharacters[j];
         clearArray();
         makeArray(arraySelect);
-        console.log(tabCharacter);
-        console.log(arrayArrayCharacters[j]);
-        console.log(j);
-
     });
     i++;
 }
